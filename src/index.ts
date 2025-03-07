ef78e88450603643e742c0e22f31ad914dca5a83
jalmonter <116984708+jalmonter@users.noreply.github.com>
2025-03-07 13:39:12 -0400

    chore: refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,87 @@
 #!/usr/bin/env node
+
 import { writeFileSync } from "fs";
 import { join } from "path";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { experimental_readRawConfig, Unstable_RawConfig } from "wrangler";
+import { name, description, homepage } from "../package.json";
 
-import { description } from "../package.json";
+const generateBanner = (name: string, homepage: string): string =>
+  `
+/**
+ * ==================================================================
+ * 🚀 This file was generated using \`${name} migrate\` command. 🚀
+ * 
+ * 📌 Learn more, contribute, or report issues:
+ *    👉 [${homepage}]
+ *     
+ * You can safely remove this banner if you want.
+ * ==================================================================
+ */`.trim();
 
 type JSONConfig = Unstable_RawConfig & { $schema: string };
 
-yargs(hideBin(process.argv))
-  .command(
-    "migrate",
-    description,
-    (yargs) => {
-      return yargs.option("to", {
+const migrateCommand = {
+  command: "migrate",
+  describe: description,
+  builder: (yargs: yargs.Argv) => {
+    return yargs
+      .option("to", {
         describe: "Target file format",
         choices: ["json", "jsonc"],
         demandOption: true,
-      }).option('save', {
-        describe: 'Save the generated JSONC file',
-        type: 'boolean',
+      })
+      .option("save", {
+        describe: "Save the generated JSON[C] file",
+        type: "boolean",
         default: false,
       });
-    },
-    (argv) => {
-      try {
-        const configPath = join(process.cwd(), "wrangler.toml");
+  },
+  handler: (argv: yargs.Arguments<{ to: string; save: boolean }>) => {
+    try {
+      const configPath = join(process.cwd(), "wrangler.toml");
 
-        if (!argv.to.includes("json")) {
-          throw new Error("The only supported target format is JSONC");
-        }
-
-        const { rawConfig } = experimental_readRawConfig({
-          config: configPath,
-        });
-
-        const jsonConfig: JSONConfig = {
-          $schema: "node_modules/wrangler/config-schema.json",
-          ...rawConfig,
-        };
-
-        if (argv.save) {
-          const outputPath = join(process.cwd(), `wrangler.${argv.to}`);
-          writeFileSync(outputPath, JSON.stringify(jsonConfig, null, 2));
-          console.log(`Configuration successfully migrated to ${outputPath}`);
-        } else {
-          console.log(JSON.stringify(jsonConfig, null, 2));
-        }
-      } catch (error: any) {
-        console.error(error.message);
-        process.exit(1);
+      if (!argv.to.includes("json")) {
+        throw new Error("The only supported target format is JSON[C]");
       }
+
+      const { rawConfig } = experimental_readRawConfig({ config: configPath });
+
+      const jsonConfig: JSONConfig = {
+        $schema: "node_modules/wrangler/config-schema.json",
+        ...rawConfig,
+      };
+
+      const fileContent =
+        generateBanner(name, homepage) +
+        "\n" +
+        JSON.stringify(jsonConfig, null, 2);
+
+      if (argv.save) {
+        const outputPath = join(process.cwd(), `wrangler.${argv.to}`);
+        writeFileSync(
+          outputPath,
+          argv.to === "jsonc"
+            ? fileContent
+            : JSON.stringify(jsonConfig, null, 2)
+        );
+        console.log(`Configuration successfully migrated to ${outputPath}`);
+      } else {
+        console.log(
+          argv.to === "jsonc"
+            ? fileContent
+            : JSON.stringify(jsonConfig, null, 2)
+        );
+      }
+    } catch (error: any) {
+      console.error(error.message);
+      process.exit(1);
     }
-  )
+  },
+};
+
+yargs(hideBin(process.argv))
+  .command(migrateCommand)
   .demandCommand(1, "You need at least one command before moving on")
   .help().argv;
